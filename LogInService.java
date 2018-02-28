42548f051f73827c3e7beae38cb49c8ce994258a
Christina Terradista <terradistacc@gmail.com>
2018-02-28 17:49:28 -0500

    test commit

---
+++ LogInService.java
@@ -0,0 +1,8 @@
+package coreservices;
+
+public class LoginService {
+    //Determine what kind of user the new user will be (doctor, medical professional, patien)
+    //if patient == true, call on the patient class
+    //if doctor == true, call on the doctor class
+    //if medical professional == true, call on the medical professional class
+}
