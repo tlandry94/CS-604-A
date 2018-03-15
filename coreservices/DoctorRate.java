2f56c4c36cac77386eb0bac75a2a49d970de1fd6
fekadul <32182593+fekadul@users.noreply.github.com>
2018-03-14 22:32:10 -0400

    Create DoctorRate.java

---
+++ coreservices/DoctorRate.java
@@ -0,0 +1,17 @@
+class DoctorRate extends User, Doctor {
+    private date rateDate;
+    private int rate;
+    
+    public getRateDate(){
+      return rateDate;
+    }
+    public getRate(){
+      return rateDate;
+    }
+    public setRateDate(){
+      return rateDate;
+    }
+    public setRate(){
+      return rateDate;
+    }
+}
